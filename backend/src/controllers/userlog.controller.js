9637d0e19c8ba43db060a9b56e7f58048c3fb74b
mikmak29 <mjogamaapple@gmail.com>
2026-01-27 21:56:10 +0800

    feature: userlog-validation-middleware

--- backend/src/controllers/userlog.controller.js
+++ backend/src/controllers/userlog.controller.js
@@ -1,40 +1,21 @@
 import asyncHandler from "express-async-handler";
 
 import * as userLogService from "../services/userLog.service.js";
-import { FILE_NAME } from "../constants/FILE_NAME.js";
-import errorHandler from "../utils/errorHandler.js";
-import validateObjectId from "../utils/validateObjectId.js";
 import ResponseHandler from "../utils/ResponseHandler.js";
 
-const {
-	controllers: { USER_LOG },
-} = FILE_NAME;
-
-export const getAllLogs = asyncHandler(async (req, res) => {
+export const getAllUserLogsADMIN = asyncHandler(async (req, res) => {
 	const data = await userLogService.fetchLogs();
-
-	if (!data) {
-		return errorHandler("No data found.", 400, USER_LOG);
-	}
 
 	ResponseHandler(res, "success", 200, {
 		message: "Retrieved User Logs",
-		data,
+		data: data || null,
 	});
 });
 
-export const getUserLogByType = asyncHandler(async (req, res) => {
-	const type = req.query.type;
-
-	if (!type) {
-		return errorHandler("Query not found.", 409, USER_LOG);
-	}
+export const getUserLogByTypeADMIN = asyncHandler(async (req, res) => {
+	const { data: type } = req;
 
 	const logsData = await userLogService.fetchLogByType(type);
-
-	if (!logsData) {
-		return errorHandler(`No type ${type} data found.`, 409, USER_LOG);
-	}
 
 	ResponseHandler(res, "success", 200, {
 		message: `Retrieve Users log by ${type}`,
@@ -43,16 +24,9 @@ export const getUserLogByType = asyncHandler(async (req, res) => {
 });
 
 export const getUserLogsById = asyncHandler(async (req, res) => {
-	const id = req.userData?.id;
-
-	console.log(id);
-	validateObjectId(id);
+	const { data: id } = req;
 
 	const logsData = await userLogService.fetchLogById(id);
-
-	if (!logsData) {
-		return errorHandler("ID not found or invalid", 409, USER_LOG);
-	}
 
 	ResponseHandler(res, "success", 200, {
 		message: "Retrieved logs",
@@ -61,16 +35,9 @@ export const getUserLogsById = asyncHandler(async (req, res) => {
 });
 
 export const deleteUserLogById = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-	const userId = req.userData?.id;
-
-	validateObjectId(id);
-
-	const user = await userLogService.ownerShip(id, userId);
-
-	if (!user) {
-		return errorHandler("No data found or you don't have permission.", 409, USER_LOG);
-	}
+	const {
+		data: { id, userId },
+	} = req;
 
 	await userLogService.deleteLogById(id, userId);
 

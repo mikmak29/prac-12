9231d131c179964cb9fddcefafd0f6e83ad11a7f
mikmak29 <mjogamaapple@gmail.com>
2026-02-06 23:18:22 +0800

    feature: route-constant-implementation

---
+++ backend/src/constants/ROUTES.js
@@ -0,0 +1,21 @@
+export const USER_ROUTE = {
+	default: "/register",
+	register: "/register",
+	login: "/login",
+	refreshToken: "/refreshToken",
+	userData: "/userData",
+	update: "/update/:id",
+};
+
+export const TRANS_ROUTE = {
+	deposit: "/deposit",
+	withdraw: "/withdraw",
+	transfer: "/transfer",
+};
+
+export const USERLOG_ROUTE = {
+	admin: "/admin",
+	current: "/current/admin",
+	user: "/user/:id",
+	del: "/del/:id",
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,13 +3,16 @@ import express from "express";
 import * as userController from "../controllers/user.controller.js";
 import { validateRegister, validateLogin, validateRefreshToken } from "../middlewares/validation/validateUser.js";
 import authToken from "../middlewares/authToken.js";
+import { USER_ROUTE } from "../constants/ROUTES.js";
 
 const route = express.Router();
 
-route.post("/register", validateRegister, userController.registerUserData);
-route.post("/login", validateLogin, userController.loginUser);
-route.post("/refreshToken", validateRefreshToken, userController.refreshToken);
-route.get("/userData", authToken, userController.currentUserData);
-route.put("/update/:id", authToken, userController.updateUserData);
+const { register, login, refreshToken, userData, update } = USER_ROUTE;
+
+route.post(`${register}`, validateRegister, userController.registerUserData);
+route.post(`${login}`, validateLogin, userController.loginUser);
+route.post(`${refreshToken}`, validateRefreshToken, userController.refreshToken);
+route.get(`${userData}`, authToken, userController.currentUserData);
+route.put(`${update}`, authToken, userController.updateUserData);
 
 export default route;
